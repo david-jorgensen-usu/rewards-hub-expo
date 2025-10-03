8d0e2d92263976998338f8f9cfb1da16e64b345b
David Jorgensen <a02365616@usu.edu>
2025-10-03 01:41:22 +0000

    Added vertical scrolling to the Card Gallery and more logos

--- app/index.jsx
+++ app/index.jsx
@@ -32,6 +32,38 @@ export default function Index() {
           companyLogo="https://www.allrecipes.com/thmb/s86K8lVB5uw18upmfu3Z2gADDzA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/allrecipes-panda-express-4x3-935b766703a145f4ab5792da94f0f3d0.jpg"
           points={30}
         />
+        <CompanyCard
+          companyLogo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTpdnCaNKupk2YSbUXA2tNGPGcNA3jTj2QyA&s"
+          points={500}
+        />
+        <CompanyCard
+          companyLogo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcEe4qZNWGZ9_w2u53w-qpkkTH1dDp3G7rHw&s"
+          points={300}
+        />
+        <CompanyCard
+          companyLogo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNFsqOKKk-INdD6cBxf8dGKqPeyILKk_qJgg&s"
+          points={20}
+        />
+        <CompanyCard
+          companyLogo="https://a.storyblok.com/f/242299/512x512/1bd930f4fe/placeholder.jpg/m/"
+          points={20}
+        />
+        <CompanyCard
+          companyLogo="https://theredledger.net/wp-content/uploads/2022/05/HcAaSiQHjWLLR1qNPq61oNbg0nJhbhS9NOy0nzzB.png"
+          points={70}
+        />
+        <CompanyCard
+          companyLogo="https://pbs.twimg.com/profile_images/1935756290506027008/MgvAA09k_400x400.png"
+          points={40}
+        />
+        <CompanyCard
+          companyLogo="https://chambermaster.blob.core.windows.net/images/members/2428/2119/MemLogo_Smiths.png"
+          points={170}
+        />
+        <CompanyCard
+          companyLogo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVEVgMwbQvcL4eBS5IGtDkBuvEImW8XvlKBtBh7s8kNfYbUT6yFdT6xGUFWVZuiJcbD2E&usqp=CAU"
+          points={220}
+        />
       </CardGallery>
     </View>
   );
--- components/global/CardGallery.jsx
+++ components/global/CardGallery.jsx
@@ -1,36 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function CardGallery({ galleryTitle, children }) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.cardGalleryTitle}>{galleryTitle}</Text>
-            <View style={styles.cardGallery}>
-                {children}
-            </View>
-        </View>
-    );
-}        
+  return (
+    <View style={styles.container}>
+      <Text style={styles.cardGalleryTitle}>{galleryTitle}</Text>
+      <ScrollView style={{ width: "100%" }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.cardGallery} // layout of children
+      >
+        {children}
+      </ScrollView>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        alignItems: 'center',
-    },
-    cardGallery: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        padding: 10,
-        gap: 5,
-        flexWrap: 'wrap',
-    },
-    cardGalleryTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 10,
-        alignSelf: 'flex-start',
-        paddingLeft: 20,
-    },
+  container: {
+    width: "100%",
+    alignItems: "center",
+    flex: 1, // ensures ScrollView can take available space
+  },
+  cardGallery: {
+    flexDirection: "row",   // stack children vertically
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+    flexWrap: "wrap",
+  },
+  cardGalleryTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+    alignSelf: "flex-start",
+    paddingLeft: 20,
+  },
 });
